baf0ab81c5a50e17bf85e87862a0257a8a425ea8
Roberto Klein <roberto.klein@locaweb.com.br>
2010-05-25 17:08:38 -0300

    Initial

---
+++ parser.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'pp'
+
+n = Nokogiri::HTML(open("https://api.snaptic.com/v1/notes.xml", :http_basic_authentication => ARGV))
+n.xpath("//note").each do |note|
+  next unless note.children.find_all { |node| node.name == "tags" }.find_all { |node| node.text == "ignore" }.empty?
+  puts "- " + note.children.find_all { |node| node.name == 'text' }.first.text
+end
