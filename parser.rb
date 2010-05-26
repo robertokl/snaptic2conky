13590ac863677735c09b0b6451593ae0fc6cbcfa
Roberto Klein <robertokl@gmail.com>
2010-05-26 08:26:51 -0300

    Arrumando filtro

--- parser.rb
+++ parser.rb
@@ -5,6 +5,6 @@ require 'pp'
 
 n = Nokogiri::HTML(open("https://api.snaptic.com/v1/notes.xml", :http_basic_authentication => ARGV))
 n.xpath("//note").each do |note|
-  next unless note.children.find_all { |node| node.name == "tags" }.find_all { |node| node.text == "ignore" }.empty?
+  next unless note.children.find_all { |node| node.name == 'tags' }.first.children.find_all {|node| node.text == "ignore"}.empty?
   puts "- " + note.children.find_all { |node| node.name == 'text' }.first.text
 end
